b7d5f4f25d86d2d47be9fa632f2b7a5b6944e1dd
Weki <wekizator@elfak.rs>
2021-11-19 16:50:57 +0100

    Prvi komit

---
+++ Lab1_Veljko_Mijovic_Uskovic_17224/Lab1_Veljko_Mijovic_Uskovic_17224/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab1_Veljko_Mijovic_Uskovic_17224
+{
+    public partial class RC4Cryptography : Form
+    {
+        string sourcePath = null;
+        string destPath= null;
+        string watchPath = null;
+        string title = null;
+        FileSystemWatcher fW = new FileSystemWatcher();
+        public RC4Cryptography()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RC4Cryptography_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fDiag = new OpenFileDialog();
+            //fDiag.Filter = "(*.txt)|(*.rc4)";
+
+            if(fDiag.ShowDialog()== DialogResult.OK)
+            {
+                sourcePath = fDiag.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var fbDiag = new FolderBrowserDialog();
+
+            if(fbDiag.ShowDialog() == DialogResult.OK)
+            {
+                destPath = fbDiag.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sourcePath == null || !sourcePath.Contains(".txt") || destPath==null)
+                return;
+
+            string plainText = System.IO.File.ReadAllText(sourcePath);
+
+            var rc4Encrypt = RC4CryptographyLab.GetInstance();
+            byte[] cyText = rc4Encrypt.Encrypt(plainText);
+
+            var bWrite = new BinaryWriter(File.Open(destPath + "\\enc1.rc4", FileMode.OpenOrCreate));
+            bWrite.Write(cyText, 0, cyText.Length);
+            bWrite.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (sourcePath == null || !sourcePath.Contains(".rc4") || destPath == null)
+                return;
+
+            byte[] cypherText = System.IO.File.ReadAllBytes(sourcePath);
+
+            var rc4Encrypt = RC4CryptographyLab.GetInstance();
+            string plainText = rc4Encrypt.Dencrypt(cypherText);
+
+            var sWrite = new StreamWriter(File.Open(destPath + "\\denc1.txt", FileMode.OpenOrCreate));
+            sWrite.Write(plainText);
+            sWrite.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (watchPath == null || destPath==null)
+                return;
+
+            if (checkBox1.Checked)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                textBox1.Enabled = false;
+                int count = 0;
+
+
+                fW.Path = watchPath;
+                fW.EnableRaisingEvents = true;
+
+                fW.Created += (object sender, FileSystemEventArgs e) =>
+                {
+                    if (!e.FullPath.Contains(".txt") || destPath == null)
+                        return;
+
+                    var rc4Encrypt = RC4CryptographyLab.GetInstance();
+                    string plainText = System.IO.File.ReadAllText(e.FullPath);
+
+                    byte[] cyText = rc4Encrypt.Encrypt(plainText);
+
+                    var bWrite = new BinaryWriter(File.Open(destPath + "\\enc" + count.ToString() + ".rc4", FileMode.OpenOrCreate));
+                    bWrite.Write(cyText, 0, cyText.Length);
+                    bWrite.Close();
+                    count++;
+                };
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                textBox1.Enabled = true;
+
+                fW.EnableRaisingEvents = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var fbDiag = new FolderBrowserDialog();
+
+            if (fbDiag.ShowDialog() == DialogResult.OK)
+            {
+                watchPath = fbDiag.SelectedPath;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var rc4Encrypt = RC4CryptographyLab.GetInstance();
+            rc4Encrypt.Key = textBox1.Text;
+        }
+    }
+}
---
+++ Lab1_Veljko_Mijovic_Uskovic_17224/Lab1_Veljko_Mijovic_Uskovic_17224/RC4CryptographyLab.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Veljko_Mijovic_Uskovic_17224
+{
+    public class RC4CryptographyLab
+    {
+        //Singleton klasa za rc4 kriptografiju
+
+        private static RC4CryptographyLab instance = null;
+        private string key;
+        private string latestData;
+        private byte[] s = new byte[256];
+
+        private RC4CryptographyLab()
+        {
+            key = null;
+            latestData = null;
+
+            for (int i = 0; i < 256; i++)
+                s[i] = (byte)i;
+        }
+
+        public static RC4CryptographyLab GetInstance() 
+        {
+            if (instance != null)
+                return instance;
+            instance = new RC4CryptographyLab();
+            return instance;
+        }
+
+        public string Key
+        {
+            get { return key; }
+            set
+            {
+                byte[] asciiByteKey = Encoding.Convert(Encoding.Unicode, Encoding.ASCII, Encoding.Unicode.GetBytes(value));
+                char[] asciiCharsKey = new char[Encoding.ASCII.GetCharCount(asciiByteKey)];
+                Encoding.ASCII.GetChars(asciiByteKey, 0, asciiCharsKey.Length);
+                key = value;
+
+                for(int i =0; i < 256; i++)
+                {
+                    s[0] = (byte)i;
+                }
+
+
+                Shuffle(asciiCharsKey);
+            }
+        }
+
+        public string LatestData
+        {
+            get { return latestData; }
+        }
+
+        private void Shuffle(char[] key)
+        {
+            int j = 0;
+
+            for (int i = 0; i < 256; i++)
+            {
+                j = (j + s[i] + key[i % key.Length]) % 256;
+
+                byte temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+
+        public byte[] Run(byte[] data)
+        {
+            if (key == null)
+                return null;
+
+
+
+            int j = 0;
+            byte[] results = new byte[data.Length];
+
+            byte[] s = new byte[256];
+            this.s.CopyTo(s, 0);
+            int i = 0;
+            byte keyCurrent;
+            byte temp;
+            for (int iData = 0; iData < data.Length; iData++)
+            {
+                i = (i + 1) % 256;
+                j = (j + s[i]) % 256;
+
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+
+
+                keyCurrent = s[(s[i] + s[j]) % 256];
+
+                results[iData] = (byte)((int)data[iData] ^ (int)keyCurrent);
+                //Console.WriteLine((char)(data[iData] ^ keyCurrent));
+
+            }
+
+            // Console.WriteLine(results.ToString());
+
+            char[] resultChars = new char[Encoding.ASCII.GetCharCount(results, 0, results.Length)];
+            Encoding.ASCII.GetChars(results, 0, results.Length, resultChars, 0);
+            this.latestData = new string(resultChars);
+            return results;
+        }
+
+        public string Dencrypt(byte[] cypherText)
+        {
+            //byte[] cypherBytesDef = Encoding.Default.GetBytes(cypherText);
+            //byte[] cypherBytes = Encoding.Convert(Encoding.Default, Encoding.ASCII, cypherBytesDef);
+
+            //   byte[] cypherBytes = Encoding.ASCII.GetBytes(cypherText);
+
+            byte[] r = Run(cypherText);
+
+            char[] resultChars = new char[Encoding.ASCII.GetCharCount(r, 0, r.Length)];
+            Encoding.ASCII.GetChars(r, 0, r.Length, resultChars, 0);
+
+            string plainText = new string(resultChars);
+            return plainText;
+        }
+
+        public byte[] Encrypt(string plainText)
+        {
+            byte[] plainBytesDef = Encoding.Default.GetBytes(plainText);
+            byte[] plainBytes = Encoding.Convert(Encoding.Default, Encoding.ASCII, plainBytesDef);
+
+            //byte[] plainBytes = Encoding.ASCII.GetBytes(plainText);
+
+            byte[] r = Run(plainBytes);
+
+            return r;
+        }
+    }
+}
